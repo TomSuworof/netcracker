edbacb51e0f053daa1f7228541387e8bc72d9da1
TomSuworof <rc1995king@gmail.com>
2021-07-30 21:01:33 +0300

    lecture 12

---
+++ src/lectures/lecture12/Main.java
@@ -0,0 +1,33 @@
+package lectures.lecture12;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        getValidation().validate(new StringBuilder());
+        //getValidation().validate(null);
+
+        int[][] nums = new int[][] {
+                {1, 2, 3, 4, 5},
+                {7, 8, 9, 10},
+                {11, 12, 13}
+        };
+
+        Stream.of(nums)
+                //.flatMap(arr -> Stream.of(arr))
+                .flatMapToInt(IntStream::of)
+                .filter(val -> val >= 5 && val <= 10)
+                .max()
+                .ifPresent(System.out::println);
+
+    }
+
+    public static Validation getValidation() {
+        return (val) -> {
+            if (val == null) {
+                throw new IllegalArgumentException();
+            }
+        };
+    }
+}
---
+++ src/lectures/lecture12/Validation.java
@@ -0,0 +1,5 @@
+package lectures.lecture12;
+
+public interface Validation {
+    void validate(Object o);
+}
