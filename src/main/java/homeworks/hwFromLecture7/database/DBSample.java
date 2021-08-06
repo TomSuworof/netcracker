17778d42d728d6bf3d7697020e5684afe5eaae55
TomSuworof <rc1995king@gmail.com>
2021-08-06 14:25:46 +0300

    zoo with database

--- src/main/java/homeworks/hwFromLecture7/database/DBSample.java
+++ src/main/java/homeworks/hwFromLecture7/database/DBSample.java
@@ -1,21 +1,10 @@
 package homeworks.hwFromLecture7.database;
 
 import homeworks.hwFromLecture7.model.Animal;
-import homeworks.hwFromLecture7.model.Species;
 import homeworks.hwFromLecture7.repositories.AnimalRepository;
 import homeworks.hwFromLecture7.repositories.AnimalRepositoryImpl;
 
-import java.sql.ResultSet;
 import java.util.Optional;
-
-/*
-        steps for initializing zoo:
-        - load species (enum)
-        - load log-events (enum)
-        - load cages (class, never changes during execution)
-
-        - start using animal checking in-out, while writing logs
-*/
 
 public class DBSample {
     public static void main(String[] args) {
@@ -24,18 +13,6 @@ public class DBSample {
 
             Optional<Animal> animal = repository.findAnimalByName("King");
             animal.ifPresent(System.out::println);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        try (ConnectionManager datasource = PostgresConnectionManager.getInstance()) {
-
-            ResultSet set = datasource.getConnection().createStatement().executeQuery("select species_name from species;");
-
-            while (set.next()) {
-                String spec = set.getString("species_name");
-                System.out.println(Species.valueOf(spec));
-            }
         } catch (Exception e) {
             e.printStackTrace();
         }
